01982dbcb3ddd2b590ee1535bfc3e8602258cc37
carleyeshield <carlo_yamzon@yahoo.com>
2023-11-11 11:44:42 +0800

    StatisticsDataScience

---
+++ main.py
@@ -0,0 +1,49 @@
+# Create the following piece of code:
+# If x > 200, print out "Big";
+# If x > 100 and x <= 200, print out "Average"; and
+# If x <= 100, print out "Small".
+# Use the if, elif, and else keywords in your code.
+
+x=99
+if x > 200:
+    print('Big')
+elif 200 >= x > 100:
+    print('Average')
+else:
+    print('Small')
+
+# Certainly! You can achieve this by traversing the array in a spiral order. Here's a Python function to do that:
+#
+# python
+# Copy code
+# def spiral_order(matrix):
+#     result = []
+#     while matrix:
+#         result += matrix.pop(0)
+#
+#         if matrix and matrix[0]:
+#             for row in matrix:
+#                 result.append(row.pop())
+#         if matrix:
+#             result += matrix.pop()[::-1]
+#             print(result)
+#         if matrix and matrix[0]:
+#             for row in matrix[::-1]:
+#                 result.append(row.pop(0))
+#     return result
+#
+# # Example usage:
+# matrix = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
+
+# result = spiral_order(matrix)
+# print(result)
+
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
