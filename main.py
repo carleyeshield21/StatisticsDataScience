c0cefb51c10befebe25ded5caf362f0704c224fe
carleyeshield <carlo_yamzon@yahoo.com>
2023-11-13 13:26:17 +0800

    Data Science

--- main.py
+++ main.py
@@ -73,9 +73,6 @@ import scipy
 import statsmodels.api as sm
 
 raw_data = pandas.read_csv('1.03.+Dummies.csv')
-raw_data
 data = raw_data.copy()
-data
 data['Attendance'] = data['Attendance'].map({'Yes': 1, 'No': 0})
-data
 print(data)
